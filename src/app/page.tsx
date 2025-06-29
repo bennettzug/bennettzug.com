3e21540f2bda7092c6bf970902cf16ee24262001
bennettzug <bennettzug@gmail.com>
2025-06-29 18:45:22 -0400

    updated site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,126 @@
-import Image from "next/image";
+// import { IoIosMail, IoLogoLinkedin, IoLogoGithub } from "react-icons/io";
+// import { FaLetterboxd } from "react-icons/fa6";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    return (
+        <div className="max-w-xl text-neutral-800 dark:text-neutral-200 mx-auto px-10 py-20 space-y-12 font-light text-sm">
+            <p className="text-xl font-bold">Bennett Zug</p>
+            <p>
+                I&apos;m a computer science student at North Carolina State
+                University.{" "}
+                <a
+                    className="underline decoration-2 decoration-neutral-300 hover:text-neutral-500 dark:decoration-neutral-700 dark:hover:text-neutral-400"
+                    href="https://bennettzug.github.io/resume/resume.pdf"
+                >
+                    Here&apos;s my résumé (PDF).
+                </a>
+            </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div className="flex flex-col gap-5">
+                <p className="font-semibold">About Me</p>
+                <p>
+                    I grew up in Raleigh, North Carolina, and currently study
+                    computer science and mathematics at NC State University. My
+                    academic interests include artificial intelligence, machine
+                    learning, and numerical methods.
+                </p>
+                <p>
+                    Outside of computer science, I&apos;m interested in music,
+                    film, urbanism and transit, and basketball.
+                </p>
+            </div>
+            <div className="flex flex-col gap-5">
+                <p className="font-semibold">Work</p>
+
+                <div className="flex flex-col gap-1">
+                    <div className="flex gap-2">
+                        <p className="font-medium">St. Timothy&apos;s School</p>
+                        <p className="text-neutral-500 dark:text-neutral-400">
+                            2018 - 2025
+                        </p>
+                    </div>
+                    <p>
+                        Managed student and teacher accounts and devices, and
+                        optimized internal IT workflows.
+                    </p>
+                </div>
+            </div>
+            <div className="flex flex-col gap-5">
+                <p className="font-semibold">Side Projects</p>
+                <div className="flex flex-col gap-1">
+                    <div className="flex gap-2">
+                        <a
+                            className="font-medium underline decoration-2 decoration-neutral-300 hover:text-neutral-500 dark:decoration-neutral-700 dark:hover:text-neutral-400"
+                            href="https://www.class-search.com"
+                        >
+                            Class Search
+                        </a>{" "}
+                        <p className="text-neutral-500 dark:text-neutral-400">
+                            2024 - Now
+                        </p>
+                    </div>
+                    <p>
+                        Built a website for students to search for classes at
+                        using semantic search (currently offline).
+                    </p>
+                </div>
+
+                <div className="flex flex-col gap-1">
+                    <div className="flex gap-2">
+                        <p className="font-medium">SimCLR</p>
+                        <p className="text-neutral-500 dark:text-neutral-400">
+                            2025
+                        </p>
+                    </div>
+                    <p>
+                        {" "}
+                        Evaluated SimCLR on STL-10 for a research project.
+                        Co-authored{" "}
+                        <a
+                            className="underline decoration-2 decoration-neutral-300 hover:text-neutral-500 dark:decoration-neutral-700 dark:hover:text-neutral-400"
+                            href="/simclr_paper.pdf"
+                        >
+                            a short paper
+                        </a>{" "}
+                        on the results.
+                    </p>
+                </div>
+            </div>
+
+            <div className="flex flex-col gap-5">
+                <p className="font-semibold">Contact Me</p>
+                <div className="sm:flex space-y-2 sm:space-y-0 w-full justify-between">
+                    <a
+                        className="flex items-center gap-1 underline decoration-2 decoration-neutral-300 hover:text-neutral-500 dark:decoration-neutral-700 dark:hover:text-neutral-400"
+                        href="mailto:bennettzug@gmail.com"
+                    >
+                        {/* <IoIosMail /> */}
+                        📧 Email
+                    </a>
+
+                    <a
+                        className="flex items-center gap-1 underline decoration-2 decoration-neutral-300 hover:text-neutral-500 dark:decoration-neutral-700 dark:hover:text-neutral-400"
+                        href="https://www.linkedin.com/in/bennett-zug/"
+                    >
+                        {/* <IoLogoLinkedin /> */}
+                        💼 LinkedIn
+                    </a>
+                    <a
+                        className="flex items-center gap-1 underline decoration-2 decoration-neutral-300 hover:text-neutral-500 dark:decoration-neutral-700 dark:hover:text-neutral-400"
+                        href="https://github.com/bennettzug"
+                    >
+                        {/* <IoLogoGithub /> */}
+                        💻 GitHub
+                    </a>
+                    <a
+                        className="flex items-center gap-1 underline decoration-2 decoration-neutral-300 hover:text-neutral-500 dark:decoration-neutral-700 dark:hover:text-neutral-400"
+                        href="https://letterboxd.com/bennettzug/"
+                    >
+                        {/* <FaLetterboxd /> */}
+                        🎬 Letterboxd
+                    </a>
+                </div>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
